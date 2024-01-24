19b02bc7cb599928db1fdf56e229dffe7f134f93
Mayte <mayte710@hotmail.com>
2024-01-24 11:52:33 +0000

    vite config name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EvaluacionFinal-Modulo3-Adalab/',
+  base: '/Wow-s-Owen-Wilson/',
 });
